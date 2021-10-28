092ec8b0e1e2f6915983c51a8138cc5eb7e12d0e
boulosk <khayam.boulos1@gmail.com>
2021-10-28 14:05:18 +0300

    My Config File

---
+++ khayambCreateConfings.sh
@@ -0,0 +1,18 @@
+
+if test ! -d ./configs;then
+
+  mkdir configs
+
+fi
+
+        if test -f ./configs/config.json;then
+
+              echo "config.json is going to be overwritten"
+
+        fi
+              cat ./config.json > ./configs/config.json
+
+
+
+
+
