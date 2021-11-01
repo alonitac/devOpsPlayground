9e638df5a3ec8fbbb9e224dbe0c4da96a0933fb0
elias <member4413@gmail.com>
2021-11-02 00:09:41 +0200

    fixing some codes (NOT DONE YET...)

--- repo_backup/elias_backup.sh
+++ repo_backup/elias_backup.sh
@@ -1,21 +1,24 @@
 source ./utils.sh
+#!/bin/bash #the first line of the bash script
 # commands used: 1 adding devopsPlayground to ubuntu by using command : git clone https://github.com/alonitac/devOpsPlayground
 #2 : compressing by: tar -czvf repo_backup.tar.gz ~/git_workspace/devOpsPlayground
-#!/bin/bash #the first line of the bash script
+
 
 #git clone  https://github.com/alonitac/devOpsPlayground #for does who don't have the repo inside their ubuntu
 
 function compress {
 tar -czvf repo_backup.tar.gz ~/DevOpsplayGound
+stat -f repo_backup
 
 echo "Back up Files  $(total_files ${url})"
 echo "Backup Dic $(total_directories ${url})"
 
   echo "By $(whoami) in Date $(date)"
 }
- if [ -f "$1"/repo_backup.tar.gz ]; then
-    echo "single.tar.gz is gonna be overwritten"
+ if [ -d $directory 'repo_backup.tar.gz'/tmp; then
+    echo "back_repo.tar.gz exist"]
   else
-    echo "backup file have been created"
+mkdir repo_back_up
+    echo "backup Directory have been created"
   fi
 
