df0226184d641473c8f1e8361a4fffefd04e8e03
Alon Itach <alon.itach@mail.huji.ac.il>
2021-11-08 13:21:06 +0200

    new change

--- fill_out_configs.py
+++ fill_out_configs.py
@@ -1,8 +1,9 @@
 import json
 
-with open('configs/config.json', 'w') as f:
+with open('configs/config2.json', 'w') as f:
     json.dump({
-        'results': "good job!"
+        'results': "good job!",
+        "results2": '2'
     }, f)
 
 
