9839cd67b70822198e2e34f2b842e78977dec9aa
Alon Itach <alon.itach@mail.huji.ac.il>
2021-11-25 11:55:19 +0200

    names histogram exercise

--- names_histogram.py
+++ names_histogram.py
@@ -1,5 +1,12 @@
 
-def get_names_list():
+def names_histogram(.....):
+    ......
+
+
+if __name__ == '__main__':
+
     with open('names.txt') as file:
         lines = file.readlines()
-    return [line.rstrip() for line in lines]
+
+    hist = names_histogram([line.rstrip() for line in lines], ignore_names=['Jordi', 'Ram'])
+    print('Done')
