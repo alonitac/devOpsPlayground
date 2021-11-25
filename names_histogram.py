a06ed325bc239b4bea8949bc8280327dfbd070cf
Alon Itach <alon.itach@mail.huji.ac.il>
2021-11-25 11:58:43 +0200

    names histogram exercise

--- names_histogram.py
+++ names_histogram.py
@@ -1,6 +1,15 @@
 
 def names_histogram(.....):
-    ......
+    """
+    Calculates how frequent each name is
+    Hint: use dictionary
+
+    :param names: list of names (strings)
+    :param ignore: (Optional) list of names to ignore
+    :return: List of tuples in the form ('John', 4)
+             Such that 'John' appears 4 times in the list
+    """
+    pass   # your code here
 
 
 if __name__ == '__main__':
@@ -8,5 +17,5 @@ if __name__ == '__main__':
     with open('names.txt') as file:
         lines = file.readlines()
 
-    hist = names_histogram([line.rstrip() for line in lines], ignore_names=['Jordi', 'Ram'])
+    hist = names_histogram([line.rstrip() for line in lines], ignore=['Jordi', 'Ram'])
     print('Done')
