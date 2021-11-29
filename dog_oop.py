870fea8edb94f41c23905db724d60b338e33964d
Alon Itach <alon.itach@mail.huji.ac.il>
2021-11-29 13:12:51 +0200

    oop example

---
+++ dog_oop.py
@@ -0,0 +1,23 @@
+class Dog:
+    def __init__(self, dog_name, dog_x, dog_y):
+        pass
+
+    def walk(self, distance_x, distance_y):
+        pass
+
+    def get_location(self):
+        pass
+
+    def say_your_name(self):
+        pass
+
+
+my_dog = Dog('sally', 0, 1)
+my_dog.walk(1, 2)
+my_dog.get_location()    # expected 1, 3
+my_dog.say_your_name()   # My name is Sally
+
+
+
+
+
