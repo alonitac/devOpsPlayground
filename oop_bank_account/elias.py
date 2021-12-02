c91e01a779a9d80d03c2f16a758080f266f43cdc
elias <member4413@gmail.com>
2021-12-02 22:02:46 +0200

    ex

---
+++ oop_bank_account/elias.py
@@ -0,0 +1,31 @@
+class Bank_Account:
+
+	def __init__(self):
+		self.balance=0
+		print("Hello Welcome to the Deposit & Withdrawal ATM Machine")
+
+
+	def deposit(self):
+		amount=float(input("Enter amount For Deposited: ")) #amount with float + input
+		self.balance += amount
+		print("\n Amount Deposited:",amount)
+
+	def withdraw(self):
+		amount = float(input("Enter amount To Withdrawn: "))
+		if self.balance>=amount:
+			self.balance-=amount
+			print("\n You Withdrew:", amount)
+		else:
+			print("\n Insufficient balance ")
+
+	def display(self):
+		print("\n Available Balance=",self.balance)
+
+
+# creating an object of class
+s = Bank_Account()
+
+# functions and objects
+s.deposit()
+s.withdraw()
+s.display()
