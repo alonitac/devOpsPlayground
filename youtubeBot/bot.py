a1958a00a9e300e6d3995f723fda9607c6abebe8
Alon Itach <alon.itach@mail.huji.ac.il>
2021-12-04 22:34:02 +0200

    youtube telegram bot code

---
+++ youtubeBot/bot.py
@@ -0,0 +1,39 @@
+from telegram.ext import Updater, MessageHandler, Filters
+from youtubeBot.utils import download_youtube_file
+
+
+class Bot:
+    def __init__(self, token):
+        updater = Updater(token, use_context=True)
+        self.update = None
+        self.context = None
+        updater.dispatcher.add_handler(MessageHandler(Filters.text, self._callback))
+        updater.start_polling()
+        print('Bot is running....')
+        updater.idle()
+
+    def _callback(self, update, context):
+        """Internal method to handle received messages"""
+        self.update = update
+        self.context = context
+        self.message_handler(update.message.text)
+
+    def message_handler(self, message):
+        """Main messages handler"""
+        self.send_text(message)
+
+    def send_video(self, file_path):
+        """Sends video to a chat"""
+        self.context.bot.send_video(chat_id=self.update.message.chat_id, video=open(file_path, 'rb'), supports_streaming=True)
+
+    def send_text(self, text):
+        """Sends text to a chat"""
+        self.update.message.reply_text(text)
+
+
+class YoutubeBot(Bot):
+    pass  # TODO your code here!
+
+
+if __name__ == '__main__':
+    Bot('YOUR TOKEN HERE')
---
+++ youtubeBot/utils.py
@@ -0,0 +1,17 @@
+from youtube_dl import YoutubeDL
+
+
+def download_youtube_file(video_name, num_results=1):
+    """
+    This function downloads the first num_results search results from Youtube
+    :param video_name: string of the video name
+    :param num_results: integer representing how many videos to download
+    :return: list of paths to your downloaded video files
+    """
+    with YoutubeDL({'format': 'bestaudio', 'noplaylist': 'True'}) as ydl:
+        videos = ydl.extract_info(f"ytsearch{num_results}:{video_name}", download=True)['entries']
+    return [ydl.prepare_filename(video) for video in videos]
+
+
+# you can run the following line just for testing:
+# downloaded_videos = download_youtube_file('let it be', num_results=2)
