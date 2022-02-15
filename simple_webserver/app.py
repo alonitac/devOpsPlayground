a55b5a000dbe30770451cd6b45774f14f31ed325
adam <ad.amer1989@gmail.com>
2022-02-15 09:45:56 -0800

    changing path to browser

--- simple_webserver/app.py
+++ simple_webserver/app.py
@@ -3,16 +3,13 @@ import os
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def hello_world():
     return "<p>Hello, World! " + os.environ['HOSTNAME'] + "</p>"
 
-
-@app.route("/<name>")
+@app.route("/")
 def hello(name):
     return f"Hello, {name}!"
-
 
 app.run(host='0.0.0.0', port=3000)
 
