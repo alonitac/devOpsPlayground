836fadaed1fdcccd34b5663bd964ac6dc737c01e
Alon Itach <alon.itach@mail.huji.ac.il>
2022-03-21 13:31:30 +0200

    k8s manifests updates

--- simple_webserver/app.py
+++ simple_webserver/app.py
@@ -1,7 +1,9 @@
-from flask import Flask
+from flask import Flask, abort
 import os
 
 app = Flask(__name__)
+
+counter = 0
 
 
 @app.route("/")
@@ -9,9 +11,15 @@ def hello_world():
     return "<p>Hello, World! " + os.environ['HOSTNAME'] + "</p>"
 
 
-@app.route("/<name>")
-def hello(name):
-    return f"Hello, {name}!"
+@app.route("/healthz")
+def hello():
+    global counter
+
+    if counter > 15:
+        return abort(400, 'not ok')
+    else:
+        counter += 1
+        return "ok"
 
 
 if __name__ == '__main__':
